dfcd154f6502654de785813099097f67beff01f5
Naresh <skmnaresh02@gmail.com>
2024-05-20 18:06:02 +0530

    fourth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { OrderInfoComponent } from './order-info/order-info.component';
+import { ConfigurationComponent } from './configuration/configuration.component';
 
 const routes: Routes = [
-  {path:'Order-Information',component:OrderInfoComponent}
+  {path:'Order-Information',component:OrderInfoComponent},{path:'configuration',component:ConfigurationComponent}
 ];
 
 @NgModule({
