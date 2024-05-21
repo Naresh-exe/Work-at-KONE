151f0f3b9b1d0e1d9e1d7f264e8d3e7dd8b187d2
Naresh <skmnaresh02@gmail.com>
2024-05-21 11:27:19 +0530

    seventh

--- src/app/delete/delete.component.ts
+++ src/app/delete/delete.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ApiService, User } from '../api.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-delete',
@@ -6,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrl: './delete.component.css'
 })
 export class DeleteComponent {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: User,private apiService: ApiService,private snackBar:MatSnackBar) { 
+  }
+  
+ 
+  confirm(){
+    this.apiService.deleteUser(this.data._id).subscribe(response=>{
+        this.apiService.removeUser.next(response)
+     })
 
+    
+    this.snackBar.open('Deleted SuccessFully','',{
+      duration:3000
+    })
+
+}
 }
--- src/app/update/update.component.ts
+++ src/app/update/update.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ApiService, User } from '../api.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-update',
@@ -6,5 +10,31 @@ import { Component } from '@angular/core';
   styleUrl: './update.component.css'
 })
 export class UpdateComponent {
+  updateinfo=new FormGroup({
+    id:new FormControl(''),
+    name:new FormControl('',Validators.required),
+       profession:new FormControl('',Validators.required),
+       age:new FormControl('',Validators.required),
+       phoneNumber:new FormControl('',Validators.required),
+       gender:new FormControl('',Validators.required)
+  }) 
+  constructor(@Inject(MAT_DIALOG_DATA) public data: User,private apiService: ApiService,private snackBar:MatSnackBar) {
+this.updateinfo.controls.id.setValue(data._id.toString());
+this.updateinfo.controls.name.setValue(data.name);
+this.updateinfo.controls.profession.setValue(data.profession);
+this.updateinfo.controls.age.setValue(data.age.toString());
+this.updateinfo.controls.phoneNumber.setValue(data.phoneNumber.toString());
+this.updateinfo.controls.gender.setValue(data.gender);
+   } 
+ y:any
+  Update(): void{
+    this.y=this.updateinfo.value
+    this.apiService.updateUser(this.y).subscribe(data=>{
+      this.apiService.updatedUser.next(data);
+      this.snackBar.open('Updated SuccessFully','',{
+        duration:3000
+      })
+      })
+    }
 
 }
